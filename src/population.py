b7d638073caa1e94047a10cafc3bddaab359a377
antoinech <smb.antoine@gmail.com>
2021-05-26 12:40:09 +0200

    Réorganisation des dossiers

--- src/population.py
+++ src/population.py
@@ -1,4 +1,8 @@
 #Fichier pour générer la population
 #Objectif : recréer une population représentative de la France par rapport à différents critères.
+import  sqlite3
 
 nb_population = 100000
+database = 
+
+def GeneratePopulation():
